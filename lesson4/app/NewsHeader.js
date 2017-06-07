84f1c5ad80ab9e1eb10b0f32e3d9236a6a9638a9
zzl81cn <zzl81cn@163.com>
2017-06-07 17:12:04 +0800

    webpack&react lesson4 add css,style loader

---
+++ lesson4/app/NewsHeader.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import imageLogo from '../assets/images/y18.gif';
+import '../assets/css/NewsHeader.css';
+
+export default class NewsHeader extends React.Component{
+	getLogo() {
+		return (
+			<div className="newsHeader-logo">
+				<a href=""><img src={imageLogo}/></a>
+			</div>
+		)
+	}
+	getTitle() {
+		return (
+			<div className="newsHeader-title">
+				<a className="newHeader-textLink" href="">Hacker News</a>
+			</div>
+		)
+	}
+	getNav() {
+		var navLinks = [
+			{name: 'new', url: 'newest'},
+			{name: 'comments', url: 'newcomments'},
+			{name: 'show', url: 'show'},
+			{name: 'ask', url: 'ask'},
+			{name: 'jobs', url: 'jobs'},
+			{name: 'submit', url: 'submit'}
+		];
+		return (
+			<div className="newsHeader-nav">
+				{
+					navLinks.map(function (navLink) {
+						return (
+							<a key={navLink.url} className="newsHeader-navLink newsHeader-textLink" href={"https://news.ycombinator.com/" + navLink.url}>{navLink.name}</a>
+						)
+					})
+				}
+			</div>
+		);
+	}
+	getLogin() {
+		return (
+			<div className="newsHeader-login">
+				<a className="newsHeader-textLink" href="https://news.ycombinator.com/login?goto=news">login</a>
+			</div>
+		);
+	}
+	render(){
+		return (
+			<div className="newsHeader">
+				{/*我是新闻列表顶部*/}
+				{this.getLogo()}
+				{this.getTitle()}
+				{this.getNav()}
+				{this.getLogo()}
+			</div>
+		);
+	}
+}
---
+++ lesson4/app/app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by zzl81cn on 2017/6/7.
+ */
+
+import React from 'react';
+import { render } from 'react-dom';
+import NewsList from './NewsList.js';
+import '../assets/css/app.css';
+
+class HelloWorld extends React.Component {
+	render() {
+		return (
+			<h1>HelloWorld</h1>
+		);
+	}
+}
+
+render(<NewsList/>, document.getElementById('content'));
