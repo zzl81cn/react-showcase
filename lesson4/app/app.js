1b3e35886e12371f70958ce36b741db33fd210b5
zzl81cn <zzl81cn@163.com>
2017-06-08 16:59:16 +0800

    webpack&react lesson4 add Promise

--- lesson4/app/app.js
+++ lesson4/app/app.js
@@ -6,27 +6,26 @@ import $ from 'jquery';
 import React from 'react';
 import { render } from 'react-dom';
 import NewsList from './NewsList.js';
+import '../assets/css/load.css';
 import '../assets/css/app.css';
 
-/*class HelloWorld extends React.Component {
-	render() {
-		return (
-			<h1>HelloWorld</h1>
-		);
-	}
-}*/
-
-// render(<NewsList/>, document.getElementById('content'));
-
 function get(url) {
+	// 将现有对象转为一个新的Promise对象并返回
 	return Promise.resolve($.ajax(url));
 }
 
 get('https://hacker-news.firebaseio.com/v0/topstories.json')
 	.then(function (stories) {
+		// 将多个 Promise 实例，包装成一个新的 Promise实例并返回
 		return Promise.all(stories.slice(0, 30).map(itemId => get('https://hacker-news.firebaseio.com/v0/item/' + itemId + '.json')));
 	}).then(function (items) {
 		render(<NewsList items={items}/>, document.getElementById('content'))
 }).catch(function (error) {
 	console.error('err', error);
 });
+
+/*get('http://localhost:8080/assets/json/result.json')
+	.then(items => {
+		render(<NewsList items={items}/>, document.getElementById('content'))
+	})
+	.catch( error => console.log('err', error));*/
