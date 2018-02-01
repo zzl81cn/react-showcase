b882863544c822437b975147cea1cb38f245ef2b
周紫龙 <zzl81cn@163.com>
2018-02-01 22:55:26 +0800

    message,datepicker

--- antd-hello/index.js
+++ antd-hello/index.js
@@ -1,6 +1,32 @@
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
+// antd
+import {DatePicker, message} from 'antd';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: ""
+    }
+  }
+  handleChange(date) {
+    message.info('您选择的日期是：' + date.toString());
+    this.setState({date: date.toString()})
+  }
+  render() {
+    return (
+      <div style={{width: 200, height: 200, margin: '100px auto'}}>
+        <DatePicker onChange={value => this.handleChange(value)} />
+        <div>
+          <p>当前日期是：{this.state.date}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <h1>HelloWorld!</h1>, document.getElementById("root")
-)
+  <App />, document.getElementById("root")
+);
