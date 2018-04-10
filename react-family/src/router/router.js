2743077f8a26161652b9fae8dd08551bb36cf680
zzl81cn <zzl81cn@163.com>
2018-04-10 11:35:15 +0800

    add router

---
+++ react-family/src/router/router.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+
+import Home from '../pages/Home/Home';
+import Page1 from '../pages/Page1/Page1';
+
