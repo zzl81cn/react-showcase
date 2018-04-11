b6b4248142fee80c405cf25d056d5fb17b026605
周紫龙 <zzl81cn@163.com>
2018-04-11 23:17:14 +0800

    add redux

---
+++ react-family/src/redux/store.js
@@ -0,0 +1,6 @@
+import {createStore} from 'redux';
+import combineReducers from './reducers'
+
+let store = createStore(combineReducers);
+
+export default store;
---
+++ react-family/src/redux/testRedux.js
@@ -0,0 +1,17 @@
+import {increment, decrement, reset} from './actions/counter';
+import store from './store';
+
+// 打印初始状态
+console.log(store.getState());
+// 每次state更新时，打印日志
+// 注意subscribe返回一个函数来注销监听器
+let unsubscribe = store.subscribe(() => {
+  console.log(store.getState())
+});
+
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(reset());
+
+// stop listener
+unsubscribe();
