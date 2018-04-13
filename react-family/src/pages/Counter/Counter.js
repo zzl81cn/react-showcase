3600dfd27d836835333c6b25687d6d39c7d9845c
zzl81cn <zzl81cn@163.com>
2018-04-13 11:17:14 +0800

    modify react-redux

--- react-family/src/pages/Counter/Counter.js
+++ react-family/src/pages/Counter/Counter.js
@@ -1,19 +1,48 @@
 import React, {Component} from 'react';
-export default class Counter extends Component {
+import {increment, decrement, reset} from '../../redux/actions/counter';
+
+import {connect} from 'react-redux';
+
+class Counter extends Component {
   render() {
     return (
       <div>
-        <div>当前计数为（redux计数）</div>
+        <div>当前计数为{this.props.counter.count}}</div>
         <button onClick={() => {
-          console.log("调用自增函数")
+          console.log("调用自增函数");
+          this.props.increment();
         }}>自增</button>
         <button onClick={() => {
-          console.log("调用自减函数")
-        }}>--</button>
+          console.log("调用自减函数");
+          this.props.decrement();
+        }}>自减</button>
         <button onClick={() => {
-          console.log("调用reset函数")
-        }}>R</button>
+          console.log("调用reset函数");
+          this.props.reset();
+        }}>重置</button>
       </div>
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => {
+      dispatch(increment())
+    },
+    decrement: () => {
+      dispatch(decrement())
+    },
+    reset: () => {
+      dispatch(reset())
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
