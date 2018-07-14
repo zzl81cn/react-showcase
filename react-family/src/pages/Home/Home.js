affc96d66653677d077a10d4ad9c8fc99b6979ee
zzl81cn <zzl81cn@163.com>
2018-07-14 18:17:50 +0800

    update

--- react-family/src/pages/Home/Home.js
+++ react-family/src/pages/Home/Home.js
@@ -13,10 +13,12 @@ export default class Home extends Component {
     })
   }
   render () {
+    let { count } = this.state;
     return (
       <div>
         This is Home.<br/>
-        当前计数：{this.state.count}<br/>
+        当前计数：{count}<br/>
+        <p>此状态在按需加载情况下不支持保存状态，只有其他依赖redux的可以实现刷新后保存状态。</p>
         <button onClick={() => this._handleClick()}>自增</button>
       </div>
     )
