cbf0b0b5e6032e80f93bdf58059e19d8c2655a23
zzl81cn <zzl81cn@163.com>
2018-07-18 22:58:43 +0800

    add antd

--- react-family/src/router/router.js
+++ react-family/src/router/router.js
@@ -9,6 +9,7 @@ import Page1 from 'bundle-loader?lazy&name=page1!pages/Page1/Page1';
 import Counter from 'bundle-loader?lazy&name=counter!pages/Counter/Counter';
 import UserInfo from 'bundle-loader?lazy&name=userInfo!pages/UserInfo/UserInfo';
 // import Topics from 'pages/Topics/Topics';
+import AntEx from 'bundle-loader?lzay&name=antd!pages/AntD/AntD';
 
 const Loading = function () {
   return <div>Loading...</div>
@@ -75,6 +76,7 @@ const getRouter = () => (
       <ul>
         <li><Link to="/">首页</Link></li>
         <li><Link to="/page1">Page1</Link></li>
+        <li><Link to="/antex">AntD</Link></li>
         <li><Link to="/counter">Counter</Link></li>
         <li><Link to="/userinfo">UserInfo</Link></li>
         <li><Link to="/topics">Topics</Link></li>
@@ -82,6 +84,7 @@ const getRouter = () => (
       <Switch>
         <Route exact path="/" component={createComponent(Home)} />
         <Route path="/page1" component={createComponent(Page1)} />
+        <Route path="/antex" component={createComponent(AntEx)} />
         <Route path="/counter" component={createComponent(Counter)}/>
         <Route path="/userinfo" component={createComponent(UserInfo)}/>
         <Route path="/topics" component={Topics} />
